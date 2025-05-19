7c3cf59a8f568dfac57026dead62081fbc892965
Eugene Murray <eugene.murray@gmail.com>
2025-05-19 15:26:02 +0100

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { PageComponent } from "./page/page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavComponent, PageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
